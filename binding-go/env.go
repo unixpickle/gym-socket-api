ea3cbd544f6f1069ef65525f20ec6a5013658445
Alex Nichol <unixpickle@gmail.com>
2017-04-22 15:40:05 -0400

    moved Go upload method out of env.go
    Upload() doesn't really belong in the file for environments.

--- binding-go/env.go
+++ binding-go/env.go
@@ -3,9 +3,7 @@ package gym
 import (
 	"bufio"
 	"encoding/json"
-	"errors"
 	"net"
-	"os"
 	"path/filepath"
 	"sync"
 
@@ -72,52 +70,6 @@ func Make(host, envName string) (env Env, err error) {
 	}
 
 	return &connEnv{Buf: rw, Conn: conn}, nil
-}
-
-// Upload connects to the API host and uses it to upload a
-// monitor directory to the Gym website.
-//
-// If the API key is "", the OPENAI_GYM_API_KEY
-// environment variable is used.
-//
-// If the directory is a relative path, it should be
-// relative to the current working directory.
-func Upload(apiHost, dir, apiKey, algorithmID string) (err error) {
-	essentials.AddCtxTo("upload monitor", &err)
-	env, err := Make(apiHost, "")
-	if err != nil {
-		return err
-	}
-	c := env.(*connEnv)
-	defer c.Close()
-
-	if apiKey == "" {
-		apiKey = os.Getenv("OPENAI_GYM_API_KEY")
-	}
-	absDir, err := filepath.Abs(dir)
-	if err != nil {
-		return err
-	}
-
-	if err := writePacketType(c.Buf, packetUpload); err != nil {
-		return err
-	}
-	for _, str := range []string{absDir, apiKey, algorithmID} {
-		if err := writeByteField(c.Buf, []byte(str)); err != nil {
-			return err
-		}
-	}
-	if err := c.Buf.Flush(); err != nil {
-		return err
-	}
-
-	if errMsg, err := readByteField(c.Buf); err != nil {
-		return err
-	} else if len(errMsg) > 0 {
-		return errors.New(string(errMsg))
-	}
-
-	return nil
 }
 
 func (c *connEnv) Reset() (obs Obs, err error) {
